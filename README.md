860d841cdcdc4c58964220a9f9a13e89fb422166
Allen Hai <allen.hai@hotmail.com>
2016-10-27 07:43:55 -0500

    update README with new TODO

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 What if we had an intuitive way to log everything we did? If we had access to the data,
 would we be able to play with it and draw insights for growth?
 
-Let's find out. 
+Let's find out.
+
+TODO: create a mastery tracker (10,000 hours per trade)
--- test/controllers/activities_controller_test.rb
+++ test/controllers/activities_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ActivitiesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should list logs" do
+    get :index
+    assert_response :success
+  end
+
+  test "should present new log form" do
+    get :new
+    assert_response :success
+  end
+  
 end
